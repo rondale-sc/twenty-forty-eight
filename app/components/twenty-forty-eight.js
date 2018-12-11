b05a7360f42adbbedfb7bc1acf3fc032f08f09b6
Jonathan Jackson <jonathan.jackson1@me.com>
2018-12-11 14:30:30 -0500

    Add event handling in TwentyFortyEight component
    Uses ember-keyboard-shortcuts more info here:
    
    https://github.com/Skalar/ember-keyboard-shortcuts

--- app/components/twenty-forty-eight.js
+++ app/components/twenty-forty-eight.js
@@ -1,4 +1,46 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service'
+import {
+  bindKeyboardShortcuts,
+  unbindKeyboardShortcuts
+} from 'ember-keyboard-shortcuts';
 
 export default Component.extend({
+  eventManager: service('event-manager'),
+
+  init() {
+    this._super(...arguments);
+
+    this.keyboardShortcuts = {
+      up: 'up',
+      down: 'down',
+      right: 'right',
+      left: 'left'
+    }
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+    bindKeyboardShortcuts(this);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    unbindKeyboardShortcuts(this);
+  },
+
+  actions: {
+    up() {
+      console.log('up');
+    },
+    down() {
+      console.log('down');
+    },
+    right() {
+      console.log('right');
+    },
+    left() {
+      console.log('left');
+    }
+  }
 });
